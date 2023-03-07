f150c138b9cdbbe21a138c1f36a75df821c5de3a
Tihomir Tenev <102279349+Ttenev89@users.noreply.github.com>
2023-03-08 00:08:12 +0200

    Add files via upload

---
+++ javaStringReverse.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class javaStringReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+        System.out.println(A.equals(new StringBuilder(A).reverse().toString()) ? "Yes" : "No");
+    }
+}
+
